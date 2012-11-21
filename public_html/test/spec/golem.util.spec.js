d9634b70c5649b9e50700c97ccde810486296c4f
Stephen <Stephen@192.168.2.8>
2012-11-21 09:41:57 -0500

    Single case unit test

--- public_html/test/spec/golem.util.spec.js
+++ public_html/test/spec/golem.util.spec.js
@@ -3,5 +3,9 @@ describe('Golem.Util', function() {
         it('verifies a property exists', function() {
             expect(Golem.Util.checkExists('prop', { prop : true })).toEqual(true);
         });
+    
+        it('verifies a property does not exist', function() {
+            expect(Golem.Util.checkExists('prop', { })).toEqual(false);
+        });
     });
 });
