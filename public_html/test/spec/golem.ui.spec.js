2f9561283331b8976b3e12332a841812d04bb8c5
Stephen <Stephen@192.168.2.8>
2012-11-21 15:28:38 -0500

    Cleaned up the unit tests to share a constructed UI object where possible.

--- public_html/test/spec/golem.ui.spec.js
+++ public_html/test/spec/golem.ui.spec.js
@@ -1,5 +1,28 @@
 describe('Golem.UI', function() {
-    var UI = new Golem.UI();
+    describe('constructor', function() {
+        var UI_NoOptions = new Golem.UI();
+        it('prepares a canvas if no options are passed in', function() {
+            expect(UI_NoOptions.canvas).toBeDefined();
+        });
+        it('uses a canvas passed in options hash instead of making a new one', function() {
+            var externalCanvas = document.createElement('canvas'),
+                UI = new Golem.UI({ canvas : externalCanvas });
+            
+            expect(UI.canvas).toBe(externalCanvas);
+        });
+        
+        it('prepares the canvas by adding the `golem-stage` class', function() {
+            expect($(UI_NoOptions.canvas).hasClass('golem-stage')).toBe(true);
+        });
+    
+        it('resizes the canvas to fit the screen', function() {
+            expect(UI_NoOptions.width).toBeGreaterThan(0);
+            expect(UI_NoOptions.height).toBeGreaterThan(0);
+            expect(UI_NoOptions.canvas.width).toBeGreaterThan(0);
+            expect(UI_NoOptions.canvas.height).toBeGreaterThan(0);
+        });
+    });
+    
     describe('createCanvas', function() {
         it('returns a canvas DOM element', function() {
             var canvas = Golem.UI.createCanvas();
