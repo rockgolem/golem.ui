bba6cd08b6a830f5216cde9b472b0fd4d039cfce
Stephen <Stephen@192.168.2.8>
2012-11-21 10:30:07 -0500

    Switched to the "toBe" syntax for ===

--- public_html/test/spec/golem.util.spec.js
+++ public_html/test/spec/golem.util.spec.js
@@ -2,22 +2,22 @@ describe('Golem.Util', function() {
     describe('checkExists utility', function() {
         console.log('Seeing a couple of "something is undefined" logs below is normal.');
         it('verifies a property exists', function() {
-            expect(Golem.Util.checkExists('prop', { prop : 'exists' })).toEqual(true);
+            expect(Golem.Util.checkExists('prop', { prop : 'exists' })).toBe(true);
         });
         it('verifies a property does not exist', function() {
-            expect(Golem.Util.checkExists('TestUndefined', { })).toEqual(false);
+            expect(Golem.Util.checkExists('TestUndefined', { })).toBe(false);
         });
         it('defaults to the window object if a context is not provided', function() {
-            expect(Golem.Util.checkExists('jasmine')).toEqual(true);
+            expect(Golem.Util.checkExists('jasmine')).toBe(true);
         });
         it('can handle dot notation to check for nested properties', function() {
-            expect(Golem.Util.checkExists('Golem.Util')).toEqual(true);
+            expect(Golem.Util.checkExists('Golem.Util')).toBe(true);
         });
         it('can handle an array of properties', function() {
-            expect(Golem.Util.checkExists(['Golem', 'jasmine'])).toEqual(true);
+            expect(Golem.Util.checkExists(['Golem', 'jasmine'])).toBe(true);
         });
         it('returns false if a single item in an array does not exist', function() {
-            expect(Golem.Util.checkExists(['Golem.Util', 'Golem.WTF'])).toEqual(false);
+            expect(Golem.Util.checkExists(['Golem.Util', 'Golem.WTF'])).toBe(false);
         });
         it('complains to the console if something does not exist', function() {
             spyOn(console, 'log');
