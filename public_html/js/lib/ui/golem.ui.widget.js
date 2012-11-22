f968b3a489ee67ff5a0dae0a54e3d1618f98840e
Stephen <Stephen@192.168.2.8>
2012-11-21 22:56:12 -0500

    Adding some item management code to collection widgets

--- public_html/js/lib/ui/golem.ui.widget.js
+++ public_html/js/lib/ui/golem.ui.widget.js
@@ -1,8 +1,8 @@
 // namespace
 this.Golem = this.Golem || {};
 
-(function(window, undefined) {
-    var Widget, Collection, Data, Menu, Utility;
+(function(window, _, undefined) {
+    var Widget, Collection, Data, Menu, Utility, ButtonBar;
     if (Golem.Util.checkExists('Golem.UI')) {
         
         /**
@@ -10,21 +10,135 @@ this.Golem = this.Golem || {};
          * Definitions for those widgets
          * 
          * @constructor
+         * @extends EventEmitter
          */
-        Widget = function() {
-            
-        };
+        Widget = function() {};
+        Widget.prototype = Object.create(Golem.Util.EventEmitter);
         
+        /**
+         * Overwrite this method to deal with rendering
+         */
+        Widget.prototype.render = function() { };
         
         /**
          * 
          * @constructor
          * @extends Widget
          */
-        Collection = function() {};
+        Collection = function() {
+            this.setDimensions(1, 4);
+            
+            // sequential list of all items in the collection
+            this.list = [];
+            
+            // used for rare cases when items overflow the container.
+            this.overflow = [];
+            
+        };
         Collection.prototype = Object.create(Widget.prototype);
         
+        /**
+         * Sets the boundaries for this collection.  If the collection has
+         * anything currently in the list, they will be reset to the beginning
+         * of the list, i.e. indexes will not be maintained.
+         * 
+         * If resizing the collection causes it to be too small to hold all of
+         * it's items, they will be pushed onto the overflow buffer.
+         * 
+         * @param {Number} rows
+         * @param {Number} columns
+         * @returns Collection
+         */
+        Collection.prototype.setDimensions = function(rows, columns) {
+            var newList, lastIndex;
+            
+            lastIndex = this.lastIndex = (rows * columns) - 1;
+            newList = _.compact(this.list);
+            this.dimensions = [rows, columns];
+            
+            if (newList.length > lastIndex) {
+                this.addToOverflow(newList.splice(lastIndex));
+            }
+            this.list = newList;
+            return this;
+        };
         
+        /**
+         * Pushes items onto the overflow buffer and emits an overflow event
+         * 
+         * @param {Mixed} items
+         * @returns {undefined}
+         */
+        Collection.prototype.addToOverflow = function(items) {
+            var overflow = this.overflow;
+            
+            if (_.isArray(items)) {
+                overflow + items;
+            } else {
+                overflow.push(items);
+            }
+            this.emit('overflow', items, overflow);
+        };
+        
+        /**
+         * Used to add things to the collection.
+         * 
+         * If index is an object, the row and column keys should *not* be zero
+         * based, for example:
+         * 
+         * row 1 [column 1, column 2]
+         * row 2 [column 1, column 2]
+         * row 3 [column 1, column 2]
+         * 
+         * @param {Mixed} item anything you want to store in the collection
+         * @param {Mixed} index a numeric index, or an object { row : #, column : # }
+         * @returns Collection
+         */
+        Collection.prototype.add = function(item, index) {
+            var list, oldItem, lastIndex, dimensions;
+            
+            lastIndex = this.lastIndex;
+            if (_.isObject(index)) {
+                dimensions = this.dimensions;
+                index = ((index.row - 1) * dimensions[1]) + index.column - 1;
+            }
+            if (index <= lastIndex) {
+                oldItem = list[index];
+                list[index] = item;
+                this.emit('add', {
+                    item : item,
+                    index : index,
+                    replaced : oldItem
+                });
+                if (oldItem) {
+                    this.emit('remove', {
+                        index : index,
+                        replaced : oldItem
+                    });
+                }
+            } else {
+                this.emit('outOfBounds', {
+                    item : item,
+                    attemptedIndex : index,
+                    LastIndex : lastIndex
+                });
+            }
+            return this;
+        };
+        
+        
+        /**
+         * 
+         * @constructor
+         * @extends Collection
+         */
+        ButtonBar = function() {};
+        ButtonBar.prototype = Object.create(Collection.prototype);
+        
+        /**
+         * Assigning the collection definitions.
+         */
+        Collection.ButtonBar = ButtonBar;
         
         /**
          * 
@@ -63,4 +177,4 @@ this.Golem = this.Golem || {};
          */
         Golem.UI.Widget = Widget;
     }
-}(this));
+}(this, _));
