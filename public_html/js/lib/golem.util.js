640b85e80cbdb6c7e8ab1e35e215dc2b2e15165a
Stephen <Stephen@192.168.2.8>
2012-11-21 20:58:19 -0500

    Added an implementation of an Event Emitter

--- public_html/js/lib/golem.util.js
+++ public_html/js/lib/golem.util.js
@@ -2,13 +2,13 @@
 this.Golem = this.Golem || {};
 
 (function(window, document, _, undefined) {
-    var Util, logIfUndefined;
-    
+    var Util, EventEmitter, logIfUndefined, separator = /\s+/;
+
     Util = {};
     Util.checkExists = function(dependencies, container, silent) {
         var value, exists;
         value = container || window;
-        
+
         if (_.isArray(dependencies)) {
             _.each(dependencies, function(single) {
                 _.each(single.split('.'), function(property) {
@@ -17,7 +17,7 @@ this.Golem = this.Golem || {};
 
                 exists = !_.isUndefined(value);
                 logIfUndefined(single, exists, silent);
-                
+
                 // need to reset value for next iteration
                 value = container || window;
                 return exists;
@@ -26,13 +26,116 @@ this.Golem = this.Golem || {};
             _.each(dependencies.split('.'), function(property) {
                 value = value[property];
             });
-            
+
             exists = !_.isUndefined(value);
             logIfUndefined(dependencies, exists, silent);
         }
         return exists;
     };
+
+    /**
+     * EventEmitter can be mixed into any object's prototype.
+     */
+    EventEmitter = {};
+
+    /**
+     * Bind a listener
+     * 
+     * @param {String} events space separated list
+     * @param {Function} callback
+     * @param {Mixed} context context for the callback.
+     * @returns {EventEmitter}
+     */
+    EventEmitter.on = function(events, callback, context) {
+        var registry, list;
+        if (_.isFunction(callback)) {
+            events = events.split(separator);
+            registry = this._eventRegistry || (this._eventRegistry = {});
+
+            _.each(registry, function(event) {
+                registry[event] || (registry[event] = []);
+                list.push(callback, context);
+            });
+        }
+        return this;
+    };
+
+    /**
+     * Remove a listener
+     * 
+     * @param {String} events space separated list
+     * @param {Function} callback
+     * @param {Mixed} context
+     * @returns {EventEmitter}
+     */
+    EventEmitter.off = function(events, callback, context) {
+        var registry, list, i;
+        registry = this._eventRegistry;
+        if (registry) {
+            if (events || callback || context) {
+                events = events ? events.split(separator) : _.keys(registry);
+
+                _.each(events, function(event) {
+                    list = registry[event];
+                    if (!list || !(callback || context)) {
+                        delete registry[event];
+                    } else {
+                        for (i = list.length - 2; i >= 0; i -= 2) {
+                            if (!(
+                                callback && list[i] !== callback ||
+                                context && list[i + 1] !== context
+                            )) {
+                                list.splice(i, 2);
+                            }
+                        }
+                    }
+                });
+            } else {
+                delete this._eventRegistry;
+            }
+        }
+        return this;
+    };
     
+    /**
+     * Trigger all bound callbacks for the specified events
+     * 
+     * @param {String} events space separated list
+     * @returns {EventEmitter}
+     */
+    EventEmitter.emit = function(events) {
+        var event, registry, list, i, length, args, all, rest;
+        
+        registry = this._eventRegistry;
+        
+        if (registry) {
+            rest = [];
+            events = events.split(separator);
+            for (i = 1, length = arguments.length; i < length; i++) {
+                rest[i - 1] = arguments[i];
+            }
+            
+            _.each(events, function() {
+                all = (registry.all || []).slice();
+                list = (registry[event] || []).slice();
+                if (list.length) {
+                    for (i = 0, length = list.length; i < length; i += 2) {
+                        list[i].apply(list[i + 1] || this, rest);
+                    }
+                }
+                if (all.length) {
+                    args = [event].concat(rest);
+                    for (i = 0, length = all.length; i < length; i += 2) {
+                        all[i].apply(all[i + 1] || this, args);
+                    }
+                }
+            });
+        }
+        return this;
+    };
+
+    Util.EventEmitter = EventEmitter;
+
     /**
      * Wrapper for a common console.log message
      * 
@@ -48,5 +151,5 @@ this.Golem = this.Golem || {};
 
 
 
-    _.extend(window.Golem, { Util : Util });
+    _.extend(window.Golem, {Util: Util});
 }(this, document, _));
