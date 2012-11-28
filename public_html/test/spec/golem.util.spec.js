cbf79947fb6a78156ff025339a4f194ae0094026
Stephen <Stephen@192.168.2.8>
2012-11-28 12:36:30 -0500

    Cleaned up some test code

--- public_html/test/spec/golem.util.spec.js
+++ public_html/test/spec/golem.util.spec.js
@@ -1,32 +1,33 @@
 describe('Golem.Util', function() {
     describe('checkExists', function() {
+        var Util = Golem.Util;
         console.log('"something is undefined" logs below are a normal part of the unit tests.');
         it('verifies a property exists', function() {
-            expect(Golem.Util.checkExists('prop', { prop : 'exists' })).toBe(true);
+            expect(Util.checkExists('prop', { prop : 'exists' })).toBe(true);
         });
         it('verifies a property does not exist', function() {
-            expect(Golem.Util.checkExists('TestUndefined', { })).toBe(false);
+            expect(Util.checkExists('TestUndefined', { })).toBe(false);
         });
         it('defaults to the window object if a context is not provided', function() {
-            expect(Golem.Util.checkExists('jasmine')).toBe(true);
+            expect(Util.checkExists('jasmine')).toBe(true);
         });
         it('can handle dot notation to check for nested properties', function() {
-            expect(Golem.Util.checkExists('Golem.Util')).toBe(true);
+            expect(Util.checkExists('Golem.Util')).toBe(true);
         });
         it('can handle an array of properties', function() {
-            expect(Golem.Util.checkExists(['Golem', 'jasmine'])).toBe(true);
+            expect(Util.checkExists(['Golem', 'jasmine'])).toBe(true);
         });
         it('returns false if a single item in an array does not exist', function() {
-            expect(Golem.Util.checkExists(['Golem.Util', 'Golem.WTF'])).toBe(false);
+            expect(Util.checkExists(['Golem.Util', 'Golem.WTF'])).toBe(false);
         });
         it('complains to the console if something does not exist', function() {
             spyOn(console, 'log');
-            Golem.Util.checkExists('WTF');
+            Util.checkExists('WTF');
             expect(console.log).toHaveBeenCalledWith('WTF is undefined.');
         });
         it('Keeps quiet if something does not exist but the silent parameter is true', function() {
             spyOn(console, 'log');
-            Golem.Util.checkExists('WTF', window, true);
+            Util.checkExists('WTF', window, true);
             expect(console.log).not.toHaveBeenCalledWith('WTF is undefined.');
         });
     });
